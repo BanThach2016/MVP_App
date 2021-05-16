2e82336ff9f964431db03919d27b4ec78cb63d46
Tong Huy <anhtong8x@gmail.com>
2021-05-16 09:31:36 +0700

    1.Nhanh master - Form MVP App - Tao project - Vao VCS/ Import into version control/ Create Git Repository ( Tao nhanh master tai local ) - Khi Commit la commit len local - Phai pull len Git.com thi moi co - VCS/Git/Pull de day len Git

---
+++ app/src/main/java/com/anhtong8x/mvp_app/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.anhtong8x.mvp_app.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.anhtong8x.mvp_app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
